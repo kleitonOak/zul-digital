94ec21dc61f805de40ad23cda7f7d0aafa31f9c7
José Kleiton da Silva <jose.kleiton.ext@dasa.com.br>
2020-11-05 15:15:20 -0300

    Project Skeleton

---
+++ src/main/java/com/zuldigital/tweet/outputmessage/OutputmessageApplication.java
@@ -0,0 +1,13 @@
+package com.zuldigital.tweet.outputmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputmessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputmessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuldigital/tweet/outputmessage/OutputmessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuldigital.tweet.outputmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputmessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
